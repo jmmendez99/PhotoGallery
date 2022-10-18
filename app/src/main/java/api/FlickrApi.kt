d7db40189f68966bf1dddb6dafb9f16ad5254377
Justin Mendez <jmmendez@cpp.edu>
2022-10-17 22:39:03 -0700

    Change @GET to request interesting photos from the Flickr REST API

--- app/src/main/java/api/FlickrApi.kt
+++ app/src/main/java/api/FlickrApi.kt
@@ -5,6 +5,12 @@ import retrofit2.http.GET
 
 interface FlickrApi {
 
-    @GET("/")
+    @GET(
+        "services/rest/?method=flickr.interestingness.getList" +
+                "&api_key=bf58752a7c10fa7d24af228022fb9208" +
+                "&format=json" +
+                "&nojsoncallback=1" +
+                "&extras=url_s"
+    )
     fun fetchContents(): Call<String>
 }
