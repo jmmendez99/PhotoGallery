1906e331ce5e5cf8a1af214c40079699526166e6
Justin Mendez <jmmendez@cpp.edu>
2022-10-25 13:41:01 -0700

    Add singleton to manage getting and setting stored queries

---
+++ app/src/main/java/com/bignerdranch/android/photogallery/QueryPreferences.kt
@@ -0,0 +1,22 @@
+package com.bignerdranch.android.photogallery
+
+import android.content.Context
+import android.preference.PreferenceManager
+
+private const val PREF_SEARCH_QUERY = "searchQuery"
+
+//singleton because we use the keyword object instead of class
+object QueryPreferences {
+
+    fun getStoredQuery(context: Context): String {
+        val prefs = PreferenceManager.getDefaultSharedPreferences(context)
+        return prefs.getString(PREF_SEARCH_QUERY, "")!!
+    }
+
+    fun setStoredQuery(context: Context, query: String) {
+        PreferenceManager.getDefaultSharedPreferences(context)
+            .edit()
+            .putString(PREF_SEARCH_QUERY, query)
+            .apply()
+    }
+}
