e6d4999475afdb62618b20a617ca46937e01494b
Justin Mendez <jmmendez@cpp.edu>
2022-10-30 20:25:44 -0700

    Save Worker state

--- app/src/main/java/com/bignerdranch/android/photogallery/QueryPreferences.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/QueryPreferences.kt
@@ -7,6 +7,7 @@ import java.util.prefs.Preferences
 
 private const val PREF_SEARCH_QUERY = "searchQuery"
 private const val PREF_LAST_RESULT_ID = "lastResultId"
+private const val PREF_IS_POLLING = "isPolling"
 
 //singleton because we use the keyword object instead of class
 object QueryPreferences {
@@ -31,6 +32,17 @@ object QueryPreferences {
     fun setLastResultId(context: Context, lastResultId: String) {
         PreferenceManager.getDefaultSharedPreferences(context).edit {
             putString(PREF_LAST_RESULT_ID, lastResultId)
+        }
+    }
+
+    fun isPolling(context: Context): Boolean {
+        return PreferenceManager.getDefaultSharedPreferences(context)
+            .getBoolean(PREF_IS_POLLING, false)
+    }
+
+    fun setPolling(context: Context, isOn: Boolean) {
+        PreferenceManager.getDefaultSharedPreferences(context).edit {
+            putBoolean(PREF_IS_POLLING, isOn)
         }
     }
 }
