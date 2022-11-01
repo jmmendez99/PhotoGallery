f7041348f72817d9c0bcf1b8dc5186d470b3df2d
Justin Mendez <jmmendez@cpp.edu>
2022-10-31 19:30:17 -0700

    Add spacing to code to make it more readable

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoPageFragment.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoPageFragment.kt
@@ -9,21 +9,28 @@ import android.webkit.WebView
 
 private const val ARG_URI = "photo_page_url"
 class PhotoPageFragment : VisibleFragment() {
+
     private lateinit var uri: Uri
     private lateinit var webView: WebView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         uri = arguments?.getParcelable(ARG_URI) ?: Uri.EMPTY
     }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_photo_page, container, false)
+
         webView = view.findViewById(R.id.web_view)
+
         return view
     }
+
     companion object {
         fun newInstance(uri: Uri): PhotoPageFragment {
             return PhotoPageFragment().apply {
